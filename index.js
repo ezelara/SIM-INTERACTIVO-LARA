998a6dfa2952c8dd6316b1916e6450909c22a50f
Ezequiel Lara <ezelara@hotmail.com>
2022-09-03 23:06:18 -0300

    commit final

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-/**FUnciones */
+/**Funciones */
+
+function ingresarNumero() {
+
+    let num = prompt("Ingrese un numero del 1 al 3 para conocer los precios");
+
+    while (num < 0 || num > 3) {
+        num = prompt("Error. Debe ingresar un numero entre 1 al 3");
+    }
+
+    return num;
+}
+
+
+
+/**Funciones */
+function mostrarPrecios(numero) {
+
+    let costo = 0;
+
+    switch (numero) {
+        case '1':
+            costo = 200;
+            alert("Costo del producto 1 es: $" + costo);
+            break;
+        case '2':
+            costo = 300;
+            alert("Costo del producto 2 es: $" + costo);
+            break;
+        case '3':
+            costo = 500;
+            alert("Costo del producto 3 es: $" + costo);
+            break;
+
+        default:
+            alert("Sin costo");
+    }
+
+    return costo;
+}
+
+let numero = ingresarNumero();
+
+mostrarPrecios(numero)
+
+
+
+
+
+
